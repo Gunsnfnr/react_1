02ab735fff0a00103eb6d4ba8e4039f17cc4d38d
Gunsnfnr <a.j.p.010203@gmail.com>
2023-03-01 17:28:45 +0300

    update

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import maks from './img/hot-dog.jpg'
+
+const Module = () => (
+    <>
+        <h1>Hello React</h1>
+        <img width={300} src={maks} />
+    </>
+) ;
+
+export default Module;
